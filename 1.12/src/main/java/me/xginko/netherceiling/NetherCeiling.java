8d1e999cd07be25417336e7322738a1b3fc69ab9
xGinko <blue@storm.localdomain>
2022-12-19 09:23:18 +0100

    first commit

---
+++ 1.12/src/main/java/me/xginko/netherceiling/NetherCeiling.java
@@ -0,0 +1,17 @@
+package me.xginko.netherceiling;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NetherCeiling extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
